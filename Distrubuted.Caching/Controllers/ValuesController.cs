1cdd43b2597e902d3727f66de13473ab3fbca29b
Can Gumus <mzyt.gms@gmail.com>
2024-11-10 16:09:28 +0300

    Add project files.

---
+++ Distrubuted.Caching/Controllers/ValuesController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Caching.Memory;
+using System.Text;
+
+namespace Distrubuted.Caching.Controllers
+{
+    [Route("api/controller")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        readonly IDistributedCache _distrubutedCache;
+
+        public ValuesController(IDistributedCache distrubutedCache)
+        {
+            _distrubutedCache = distrubutedCache; 
+        }
+
+        [HttpGet("set")]
+        public async Task<IActionResult> Set(string name, string surname)
+        {
+            await _distrubutedCache.SetStringAsync("name", name, options: new()
+            {
+                AbsoluteExpiration = DateTime.Now.AddSeconds(30),
+                SlidingExpiration = TimeSpan.FromSeconds(5),
+            });
+
+            await _distrubutedCache.SetAsync("surname", Encoding.UTF8.GetBytes(surname), options: new()
+            {
+                AbsoluteExpiration = DateTime.Now.AddSeconds(30),
+                SlidingExpiration = TimeSpan.FromSeconds(5),
+            });
+
+            return Ok();
+        }
+
+        [HttpGet("get")]
+        public async Task<IActionResult> Get()
+        {
+            var name = await _distrubutedCache.GetStringAsync("name");
+
+            var surnameBinary = await _distrubutedCache.GetAsync("surname");
+            var surname = Encoding.UTF8.GetString(surnameBinary);
+
+            return Ok(new
+            {
+                name,
+                surname,
+                surnameBinary
+            });
+        }
+    }
+}
---
+++ InMemory.Caching/Controllers/ValuesController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace InMemory.Caching.Controllers
+{
+    [Route("api/controller")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        readonly IMemoryCache _memoryCache;
+
+        public ValuesController(IMemoryCache memoryCache)
+        {
+            _memoryCache = memoryCache; 
+        }
+
+        //[HttpGet("set")]
+        //public void Set(string name)
+        //{
+        //    _memoryCache.Set("Name", name);
+
+        //    _memoryCache.Set("Name", name, options: new()
+        //    {
+
+        //    });
+        //}
+
+        //[HttpGet("get")]
+        //public string Get()
+        //{
+        //    if (_memoryCache.TryGetValue<string>("Name", out string name))
+        //    {
+        //        return name;
+        //    }
+        //    else
+        //    {
+        //        return "Value is null";
+        //    }
+        //}
+
+        [HttpGet("set")]
+        public void SetDate()
+        {
+            _memoryCache.Set<DateTime>("date", DateTime.Now, options: new()
+            {
+                AbsoluteExpiration = DateTime.Now.AddSeconds(30),
+                SlidingExpiration = TimeSpan.FromSeconds(5)
+            });
+        }
+
+        [HttpGet("get")]
+        public DateTime GetDate()
+        {
+            return _memoryCache.Get<DateTime>("date");
+        }
+    }
+}
