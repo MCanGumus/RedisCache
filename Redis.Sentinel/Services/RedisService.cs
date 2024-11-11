ce0e11c0734ffba3d2c69c5ad67c4daf2c66872a
Can Gumus <mzyt.gms@gmail.com>
2024-11-11 20:05:24 +0300

    Redis Sentinel logic is finished.

---
+++ Redis.Sentinel/Services/RedisService.cs
@@ -0,0 +1,57 @@
+﻿using StackExchange.Redis;
+
+namespace Redis.Sentinel.Services
+{
+    public class RedisService
+    {
+        static ConfigurationOptions sentinelOptions => new()
+        {
+            EndPoints =
+            {
+                {"localhost", 6383 },
+                {"localhost", 6384 },
+                {"localhost", 6385 }
+            },
+            CommandMap = CommandMap.Sentinel,
+            AbortOnConnectFail = false
+        };
+
+        static ConfigurationOptions masterOptions => new()
+        {
+            AbortOnConnectFail = false
+        };
+
+        static public async Task<IDatabase> RedisMasterDatabase()
+        {
+            ConnectionMultiplexer sentinelConn = await ConnectionMultiplexer.SentinelConnectAsync(sentinelOptions);
+
+            System.Net.EndPoint masterEndPoint = null;
+
+            foreach (System.Net.EndPoint endpoint in sentinelConn.GetEndPoints())
+            {
+                IServer server = sentinelConn.GetServer(endpoint);
+
+                if (!server.IsConnected)
+                    continue;
+
+                masterEndPoint = await server.SentinelGetMasterAddressByNameAsync("mymaster");//sentinel.conf dosyasındaki sentinelların master ismi...
+
+                break;
+            }
+
+            var localMasterIp = masterEndPoint.ToString() switch
+            {
+                "172.18.0.8:6379" => "localhost:6379",
+                "172.18.0.7:6379" => "localhost:6380",
+                "172.18.0.6:6379" => "localhost:6381",
+                "172.18.0.5:6379" => "localhost:6382",
+            };
+
+            ConnectionMultiplexer masterConnection = await ConnectionMultiplexer.ConnectAsync(localMasterIp);
+
+            IDatabase database = masterConnection.GetDatabase();
+
+            return database;
+        }
+    }
+}
